1d00206af601e8485af6976bb0c845c143a55158
buhee1029 <buhee1029@gmail.com>
2023-12-13 00:40:11 +0900

    평균구하기.java

---
+++ programmers/평균구하기.java
@@ -0,0 +1,17 @@
+package programmers;
+
+import java.util.Arrays;
+
+public class 평균구하기 {
+    public static void main(String[] args) {
+        int[] arr1 = {1, 2, 3, 4};
+        int[] arr2 = {5, 5};
+
+        System.out.println(solution(arr1));
+        System.out.println(solution(arr2));
+    }
+
+    public static double solution(int[] arr) {
+        return Arrays.stream(arr).average().orElse(0.0);
+    }
+}

35695c97d496fca9efdb9956f724985b8896d97a
buhee1029 <buhee1029@gmail.com>
2023-12-14 23:59:48 +0900

    서울에서김서방찾기.java

---
+++ programmers/level1/서울에서김서방찾기.java
@@ -0,0 +1,18 @@
+package programmers.level1;
+
+public class 서울에서김서방찾기 {
+    public static void main(String[] args) {
+        String[] seoul = {"Jane", "Kim"};
+        System.out.println(solution(seoul));
+    }
+
+    public static String solution(String[] seoul) {
+        for (int i = 0; i < seoul.length; i++) {
+            if ("Kim".equals(seoul[i])) {
+                return "김서방은 " + i + "에 있다";
+            }
+        }
+        return null;
+    }
+
+}

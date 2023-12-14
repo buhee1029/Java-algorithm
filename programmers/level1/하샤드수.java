3cd86976fe524c8ccf6e9440b07c7cde5c9f1bd0
buhee1029 <buhee1029@gmail.com>
2023-12-14 23:30:23 +0900

    하샤드수.java

---
+++ programmers/level1/하샤드수.java
@@ -0,0 +1,25 @@
+package programmers.level1;
+
+public class 하샤드수 {
+    public static void main(String[] args) {
+        System.out.println(solution(10));
+        System.out.println(solution(12));
+        System.out.println(solution(11));
+        System.out.println(solution(13));
+    }
+
+    public static boolean solution(int x) {
+        return x % calculateDigitSum(x) == 0;
+    }
+
+    public static int calculateDigitSum(int x) {
+        int sum = 0;
+
+        while (x > 0) {
+            sum += x % 10;
+            x /= 10;
+        }
+
+        return sum;
+    }
+}
